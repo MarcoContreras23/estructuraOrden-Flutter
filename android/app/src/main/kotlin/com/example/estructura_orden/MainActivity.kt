d4135d4a38862a616d41febad373e4cd4da8d6f2
MarcoContreras23 <marcocontreras2309@gmail.com>
2022-03-08 18:13:25 -0500

    first

---
+++ android/app/src/main/kotlin/com/example/estructura_orden/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estructura_orden
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child:Text("Hello", textDirection: TextDirection.ltr),
+    );
+  }
+}
